ea85740d634b7cc8718d667081d826cd15e08d33
Navneet Nagpal <navneetnaagpal@gmail.com>
2024-02-01 12:27:03 +0530

    Fixed dateselector

--- libs/web-react/src/components/DateSelector/index.js
+++ libs/web-react/src/components/DateSelector/index.js
@@ -1,12 +1,2 @@
-import React from 'react';
-import Loading from '../Loading';
-const DateSelector = React.lazy(() => import('./DateSelector'));
-
-const DTEComponent = (props) => {
-  return (
-    <React.Suspense fallback={<Loading />}>
-      <DateSelector {...props} />
-    </React.Suspense>
-  );
-};
-export default DTEComponent;
+import DateSelector from './DateSelector';
+export default DateSelector;
