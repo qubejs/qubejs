2bc6edcde54032034ea34e41fe30ada0db4e2abe
Navneet Nagpal <navneetnaagpal@gmail.com>
2024-03-05 16:42:27 +0530

    Added plugin architecture

--- libs/web-react/src/plugins.ts
+++ libs/web-react/src/plugins.ts
@@ -1,10 +1,25 @@
-import { storage } from './utils';
+import { customHooks } from './redux/content';
+import { storage, format } from './utils';
 
 const register = (plugin = {}) => {
-  const { components = {}, templates = {}, containers = {} }: any = plugin;
+  const {
+    components = {},
+    templates = {},
+    containers = {},
+    hooks = {},
+    formatters = {},
+    icons = {},
+    dynamicComponents = {},
+  }: any = plugin;
   storage.components.set(components);
   storage.containers.set(templates);
   storage.containers.set(containers);
+  Object.keys(hooks).forEach((key) => {
+    customHooks.add(key, hooks[key]);
+  });
+  formatters && format.setFormatters(formatters);
+  icons && storage.icons.set(icons);
+  dynamicComponents && storage.dynamicComponents.set(dynamicComponents);
 };
 /* export module */
 export { register };
