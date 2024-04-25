595bd5ec6a6777eef5b4831d1a898072b961527c
Navneet Nagpal <navneetnaagpal@gmail.com>
2024-04-25 09:19:29 +0530

    Fixed unit tests

--- apps/web/src/app/app.spec.tsx
+++ apps/web/src/app/app.spec.tsx
@@ -1,8 +1,10 @@
 import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
+import { Provider } from 'react-redux';
 import * as ui from '@qubejs/ui-material-base';
 import { Router } from 'react-router-dom';
 import { plugins } from '@qubejs/web-react';
+import { store } from '../redux';
 import App from './app';
 
 plugins.register(ui);
@@ -12,7 +14,9 @@ describe('App', () => {
     const history = createMemoryHistory({ initialEntries: ['/content/home'] });
     const { baseElement } = render(
       <Router location={history.location} navigator={history}>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </Router>
     );
     expect(baseElement).toBeTruthy();
