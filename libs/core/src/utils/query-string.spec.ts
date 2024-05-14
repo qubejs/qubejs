e9ac730f548be67a950d8cf3416208b22e007dbb
Navneet Nagpal <navneetnaagpal@gmail.com>
2024-05-14 09:46:48 +0530

    Fixed querystring

--- libs/core/src/utils/query-string.spec.ts
+++ libs/core/src/utils/query-string.spec.ts
@@ -1,32 +1,73 @@
 import QueryString from './query-string';
 
-describe('Utils:QueryString', () => {
-  describe('toString()', () => {
-    it('should return params in string with ?', () => {
-      expect(
-        new QueryString({
-          x: '1',
-          y: '2',
-        }).toString()
-      ).toBe('?x=1&y=2');
-    });
-    it('should return blank string if there is no params', () => {
-      expect(new QueryString({}).toString()).toBe('');
+describe('utils:QueryString', () => {
+  describe('QueryString()', function () {
+    test('should have defined', async () => {
+      expect(QueryString).toBeDefined();
     });
   });
-  describe('toObject()', () => {
-    it('should convert from string to object', () => {
-      expect(new QueryString('?x=1&y=2').toObject()).toEqual({
-        x: '1',
-        y: '2',
+  describe('toString()', function () {
+    let qs;
+    beforeEach(() => {
+      qs = new QueryString({
+        ok: 1,
+        talk: 'got',
       });
     });
-    it('should return blank object if there is no params', () => {
-      expect(new QueryString({}).toObject()).toEqual({});
+    test('should return string with separated', async () => {
+      expect(qs.toString()).toBe('?ok=1&talk=got');
     });
-    it('should be able to handle full url', () => {
-      expect(new QueryString('http://gssg.com/?query=1233').toObject()).toEqual({
-        query: '1233',
+  });
+  describe('toObject()', function () {
+    let qs;
+    beforeEach(() => {
+      qs = new QueryString('?pike=123&talk=more');
+    });
+    test('should convert to object', async () => {
+      expect(qs.toObject()).toEqual({
+        pike: '123',
+        talk: 'more',
+      });
+    });
+  });
+  describe('toObject() should return array', function () {
+    let qs;
+    beforeEach(() => {
+      qs = new QueryString('?pike=123&talk=more&talk=123');
+    });
+    test('should convert to object including talk as array', async () => {
+      expect(qs.toObject()).toEqual({
+        pike: '123',
+        talk: ['more', '123'],
+      });
+    });
+  });
+
+  describe('toObject() should return o: prefixed including object', function () {
+    let qs;
+    beforeEach(() => {
+      qs = new QueryString(
+        '?pike=123&talk=o%3A%7B%22a%22%3A%22234%22%2C%22b%22%3A%22w%22%7D'
+      );
+    });
+    test('should convert to object including talk as object', async () => {
+      expect(qs.toObject()).toEqual({
+        pike: '123',
+        talk: { a: '234', b: 'w' },
+      });
+    });
+  });
+  describe('toObject() should return force array', function () {
+    let qs;
+    beforeEach(() => {
+      qs = new QueryString(
+        '?pike=123&talk=o%3A%5B%22123%22%2C%22344%22%5D'
+      );
+    });
+    test('should convert to array including talk as array', async () => {
+      expect(qs.toObject()).toEqual({
+        pike: '123',
+        talk: ['123','344'],
       });
     });
   });
--- libs/core/src/utils/query-string.ts
+++ libs/core/src/utils/query-string.ts
@@ -1,3 +1,5 @@
+import common from "./common";
+
 class QueryString {
   input: any;
   isString: boolean;
@@ -12,7 +14,15 @@ class QueryString {
     let str = '';
     if (this.isObject) {
       Object.keys(this.input).forEach((key) => {
-        if (this.input[key]) {
+        if (Array.isArray(this.input[key])) {
+          this.input[key].forEach((itemVal) => {
+            str +=
+            (str ? '&' : '') + key + '=' + encodeURIComponent(itemVal);
+          })
+        } else if (typeof this.input[key] === 'object' && this.input[key] !== null) {
+          str +=
+            (str ? '&' : '') + key + '=' + encodeURIComponent('o:' + JSON.stringify(this.input[key]));
+        } else if (!common.isNullOrUndefinedBlank(this.input[key])) {
           str +=
             (str ? '&' : '') + key + '=' + encodeURIComponent(this.input[key]);
         }
@@ -22,12 +32,23 @@ class QueryString {
   }
 
   toObject() {
-    const obj: any = {};
+    const obj:any = {};
     if (this.isString) {
-      const runInput = this.input.substr(this.input.indexOf('?')) ? this.input.substr(this.input.indexOf('?') + 1) : '';
-      runInput.split('&').forEach((keyValue: string) => {
+      const runInput = this.input.substr(this.input.indexOf('?') + 1);
+      runInput.split('&').forEach((keyValue) => {
         const split = keyValue.split('=');
-        obj[split[0]] = decodeURIComponent(split[1]);
+        const actualVal = decodeURIComponent(split[1]);
+        if (actualVal.substr(0,2) === 'o:') {
+           obj[split[0]] = JSON.parse(decodeURIComponent(actualVal.substr(2)));
+        } else {
+          if (Array.isArray(obj[split[0]])) {
+            obj[split[0]].push(decodeURIComponent(split[1]))
+          } else if (obj[split[0]]) {
+            obj[split[0]] = [obj[split[0]], decodeURIComponent(split[1])];
+          } else {
+            obj[split[0]] = decodeURIComponent(split[1]);
+          }
+        }
       });
     }
     return obj;
@@ -35,3 +56,35 @@ class QueryString {
 }
 
 export default QueryString;
+
+// class QueryString {
+//   constructor(params) {
+//     this.input = params;
+//     this.isString = !!params && typeof params === 'string';
+//     this.isObject = !!params && typeof params === 'object';
+//   }
+
+  
+
+//   toObject() {
+//     var obj = {};
+//     if (this.isString) {
+//       const runInput = this.input.substr(this.input.indexOf('?') + 1);
+//       runInput.split('&').forEach((keyValue) => {
+//         const split = keyValue.split('=');
+//         if (split[1].substr(0,2) === 'o:') {
+//            obj[split[0]] = JSON.parse(decodeURIComponent(split[1].substr(2)));
+//         } else {
+//           if (Array.isArray(obj[split[0]])) {
+//             obj[split[0]].push(decodeURIComponent(split[1]))
+//           } else if (obj[split[0]]) {
+//             obj[split[0]] = [obj[split[0]], decodeURIComponent(split[1])];
+//           } else {
+//             obj[split[0]] = decodeURIComponent(split[1]);
+//           }
+//         }
+//       });
+//     }
+//     return obj;
+//   }
+// }
