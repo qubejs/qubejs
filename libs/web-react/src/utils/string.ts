2aef7055eb25df552a1b5414bd28fedb3f8ff9d8
Navneet Nagpal <navneetnaagpal@gmail.com>
2024-05-31 11:36:13 +0530

    Fixed

--- libs/web-react/src/utils/string.ts
+++ libs/web-react/src/utils/string.ts
@@ -2,13 +2,13 @@ import common from './common';
 
 const string = {
   titleCase: (val) => {
-    return !common.isNullOrUndefined(val?.trim()) ? (val.trim()[0].toUpperCase() + val.trim().substr(1).toLowerCase()) : '';
+    return !common.isNullOrUndefined(val?.trim()) && typeof val === 'string' ? (val.trim()[0].toUpperCase() + val.trim().substr(1).toLowerCase()) : '';
   },
   getFirstChar: (val) => {
-    return !common.isNullOrUndefined(val) ? val.trim()[0] : '';
+    return !common.isNullOrUndefined(val) && typeof val === 'string' ? val.trim()[0] : '';
   },
   getTwoChars: (val) => {
-    if (!common.isNullOrUndefined(val)) {
+    if (!common.isNullOrUndefined(val) && typeof val === 'string') {
       const array = val.split(' ').filter((i) => !!i);
       return ((array[0] && array[0][0]) || '') + ((array[1] && array[1][0]) || '');
     }
